14a3894ae8cddec27cef352592182321adf169e8
Kurt Strecker <ksphotopro@gmail.com>
2017-04-08 16:42:45 -0400

    starting fresh with a scrollheight approach

--- kstrckr-prototype/app.js
+++ kstrckr-prototype/app.js
@@ -10,16 +10,3 @@ document.getElementsByClassName("info-button")[0].addEventListener("click", func
         this.setAttribute("src", "icons/info-icon.jpg");
     }
 })
-
-document.getElementsByClassName("info-button")[1].addEventListener("click", function(){
-    console.log(this.parentNode.parentNode);
-    if(this.parentNode.parentNode.dataset.status === "closed"){
-        this.parentNode.parentNode.style.height = "81%";
-        this.parentNode.parentNode.dataset.status = "open";
-        this.setAttribute("src", "icons/close-icon.jpg");
-    } else if (this.parentNode.parentNode.dataset.status === "open"){
-        this.parentNode.parentNode.style.height = "19%";
-        this.parentNode.parentNode.dataset.status = "closed"
-        this.setAttribute("src", "icons/info-icon.jpg");
-    }
-})
