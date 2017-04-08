19681cedb08a2e02f12083533c590bad7eeaa5a8
Kurt Strecker <ksphotopro@gmail.com>
2017-04-07 23:37:47 -0400

    working transition showing overflow info via button

--- kstrckr-prototype/app.js
+++ kstrckr-prototype/app.js
@@ -1,1 +1,5 @@
-console.log("hi!");
+
+document.getElementById("button-holder").children[0].addEventListener("click", function(){
+    console.log("Pressed");
+    document.getElementsByClassName("tour-stop")[0].style.height = "100%";
+})
