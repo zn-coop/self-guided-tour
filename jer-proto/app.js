815ae3e21d665f8ea096f36c119a0bfbc4d02740
Jeremiah Bushau <exjerlock@hotmail.com>
2017-04-09 20:16:17 -0400

    Implemented map and exit icons

--- jer-proto/app.js
+++ jer-proto/app.js
@@ -7,7 +7,8 @@ function initMap() {
   var directionsDisplay = new google.maps.DirectionsRenderer;
 
   var map = new google.maps.Map(document.getElementById('map'), {
-    scrollwheel: false
+    scrollwheel: false,
+    fullscreenControl: false
   });
 
   directionsDisplay.setMap(map);
@@ -31,15 +32,18 @@ function initMap() {
   }
 }
 
-$('#btn').on('click', function() {
-  if (!($('#wrapper').hasClass('invisible'))) {
+$('#map-btn').on('click', function() {
+  if (!$('#wrapper').hasClass('invisible')) {
     // http://stackoverflow.com/questions/2510115/jquery-can-i-call-delay-between-addclass-and-such#2510255
     // http://stackoverflow.com/questions/1058158/what-are-queues-in-jquery <-- great read.
+    $('#map-btn').css('background-image', 'url(' + "http://images.clipartpanda.com/google-map-icon-map-8.png" + ')');
     $('#wrapper').addClass('invisible').delay(250).queue(function() {
       // could use display: none instead to avoid z-index if needed
       $(this).css('z-index', '-1').dequeue();
+
     });
   } else {
     $('#wrapper').css('z-index', '1').removeClass('invisible');
+    $('#map-btn').css('background-image', 'url(' + 'https://cdn3.iconfinder.com/data/icons/unicons-vector-icons-pack/32/exit-512.png' + ')');
   }
 });
