16efdf43022ac8172dd037542cf98c7c45ad21f0
Sarah Kiino <sarah.kiino@gmail.com>
2017-04-11 18:17:05 -0400

    edited landing page

--- kstrckr-prototype/app.js
+++ kstrckr-prototype/app.js
@@ -1,18 +1,1 @@
-const displayStopInfo = function(){
-    let targetInfo = this.parentNode.parentNode.children[1];
-    let targetInfoHeight = targetInfo.scrollHeight;
-    
-    if (targetInfo.dataset.status === "closed"){
-        targetInfo.style.height = targetInfoHeight + "px";
-        targetInfo.dataset.status = "open";
-        this.setAttribute("src", "icons/close-icon.jpg");
-    } else {
-        targetInfo.style.height = "0px";
-        targetInfo.dataset.status = "closed";
-        this.setAttribute("src", "icons/info-icon.jpg");
-    }
-}
-
-document.getElementsByClassName("info-button")[0].addEventListener("click", displayStopInfo);
-
-document.getElementsByClassName("info-button")[1].addEventListener("click", displayStopInfo);
+console.log("hi!");
